d5ff4218bf4e4ec68d0008afcdf25031b08d51ad
afromarine@afromarine-VirtualBox <dan@gmail.com>
2022-10-20 20:09:22 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+Console.WriteLine("Enter the number");
+int number=int.Parse(Console.ReadLine());
+Console.WriteLine(GetSum(number));
+
+int GetSum (int sum)
+{  
+ int res=0;
+    
+    while (sum>0)
+    {
+        res=res+ sum%10;
+        sum=sum/10;
+
+        }
+    
+    return res;
+    }
+
